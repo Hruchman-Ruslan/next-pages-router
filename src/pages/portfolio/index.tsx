a01497b915695ea3f6372ad14d2b2f70c8e05c9a
Ruslan <Giovingo92@gmail.com>
2024-06-14 16:22:43 +0300

    feat: nested path

---
+++ src/pages/portfolio/index.tsx
@@ -0,0 +1,9 @@
+export interface PortfolioPageProps {}
+
+export default function PortfolioPage({}: PortfolioPageProps) {
+  return (
+    <div>
+      <h1>The Portfolio Page</h1>
+    </div>
+  );
+}
---
+++ src/pages/portfolio/list.tsx
@@ -0,0 +1,9 @@
+export interface ListPageProps {}
+
+export default function ListPage({}: ListPageProps) {
+  return (
+    <div>
+      <h1>The List Page</h1>
+    </div>
+  );
+}
